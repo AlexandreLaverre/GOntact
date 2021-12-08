91e330adb1088ec96d6593771ca85a34c845e506
Alexandre Laverré <alexandre.laverre@univ-lyon1.fr>
2021-12-08 18:32:00 +0100

    Adapt to gene annotation format

--- baits.GO.annotation.py
+++ baits.GO.annotation.py
@@ -2,26 +2,26 @@
 # coding=utf-8
 
 import argparse
+from collections import defaultdict
 
 parser = argparse.ArgumentParser()
 parser.add_argument("species", help="species")
-parser.add_argument("--unique", action="store_true",
-                    help="Get unique list of GO term associated to overlapping genes (default=complete)")
+parser.add_argument("--UniqueGO", action="store_true",
+                    help="Get unique list of GO term associated to genes for each bait (default=complete)")
 args = parser.parse_args()
 
-################################################
+######################################################################
 
 GenomeAssembly = "hg38" if args.species == "human" else "mm10"
-Prefix = "unique" if args.unique else "complete"
+Prefix = "unique" if args.UniqueGO else "complete"
 
 path = "/home/laverre/GOntact"
 Baits = path + "/data/PCHi-C/" + args.species + "/bait_coords_" + GenomeAssembly + ".txt"
 EnsemblAnnotation = path + "/data/ensembl_annotations/" + args.species + "/GeneNames_Ensembl94.txt"
-AnnotatedGenes = path + "/results/"
+AnnotatedGenes = path + "/data/GeneOntology/tmp.genes.annotation.txt"
 OutputFile = path + "/results/" + args.species + "/" + Prefix + ".GO.annotated.baits.txt"
 
-################################################
-
+######################################################################
 ## Correspondence between EnsemblID and GeneName
 GeneID_dic = {}
 with open(EnsemblAnnotation, 'r') as f:
@@ -33,20 +33,22 @@ with open(EnsemblAnnotation, 'r') as f:
 
         GeneID_dic[GeneID] = GeneName
 
+######################################################################
 ## GO Annotations of each genes
-GeneGO_dic = {}
+GeneGO_dic = defaultdict(list)
 with open(AnnotatedGenes, 'r') as f:
     for gene in f.readlines()[1:]:
         gene = gene.strip("\n")
         gene = gene.split("\t")
         GeneName = gene[0]
-        GOTerms = gene[1].split(",")
+        GOTerm = gene[1]
 
-        GeneGO_dic[GeneName] = [GO for GO in GOTerms]
+        GeneGO_dic[GeneName].append(GOTerm)
 
+######################################################################
 ## Annotations of each bait & writing Output
 Output = open(OutputFile, "w")
-Output.write("BaitID\tGeneID\tGeneName\tGOID")
+Output.write("BaitID\tGeneID\tGeneName\tGOID\n")
 
 with open(Baits, 'r') as f:
     for bait in f.readlines()[1:]:
@@ -61,7 +63,7 @@ with open(Baits, 'r') as f:
         if args.unique:
             GenesGOTerm = list(set(GenesGOTerm))
 
-        Output.write(BaitID + "\t" + ",".join(BaitGenesID) + "\t" + ",".join(GenesNames) + "\t" + ",".join(GenesGOTerm))
+        Output.write(BaitID + "\t" + ",".join(BaitGenesID) + "\t" + ",".join(GenesNames) + "\t" + ",".join(GenesGOTerm) + '\n')
 
 Output.close()
 
