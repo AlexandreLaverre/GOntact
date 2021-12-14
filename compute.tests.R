ee50ee501cc1ce87f340795873f7b786bc1227b9
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-12-14 07:31:00 +0100

    Correct loading of full dataset

--- compute.tests.R
+++ compute.tests.R
@@ -1,10 +1,14 @@
+sp = "human"
+enhancers = "FANTOM5.brain.enhancers.hg38"
+GOtype = "complete"
+
 ## Get the total Foreground & Background count in the header
-HeadFile <- file("/home/laverre/GOntact/results/human/unique.GO.frequency.txt","r")
+HeadFile <- file(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency.txt"),"r")
 ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 close(HeadFile)
 
-GOEnrichment = read.table("/home/laverre/GOntact/results/human/unique.GO.frequency.txt", h=T)
+GOEnrichment = read.table(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency.txt"), h=T, quote="", sep="\t")
 
 # Hypergeometric test
 GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
