ae0c4f61ca07b02105cb5280a51a471e7a8a2a4c
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-12-15 22:35:54 +0100

    input options

--- compute.tests.R
+++ compute.tests.R
@@ -1,14 +1,17 @@
 sp = "human"
-enhancers = "FANTOM5.brain.enhancers.hg38"
+enhancers = "FANTOM5.first150.Tcell.hg38" #"FANTOM5.first150.neuron.enhancers.hg38" 
 GOtype = "complete"
+Baited = ".BaitedEnh"
+Trans =  "" #".Trans"
+bait = "" # ".bait2bait"
 
 ## Get the total Foreground & Background count in the header
-HeadFile <- file(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency.txt"),"r")
+HeadFile <- file(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"),"r")
 ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 close(HeadFile)
 
-GOEnrichment = read.table(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency.txt"), h=T, quote="", sep="\t")
+GOEnrichment = read.table(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"), h=T, quote="", sep="\t")
 
 # Hypergeometric test
 GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
@@ -17,3 +20,4 @@ GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["Fore
 GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
 
 
+GOEnrichment.default = GOEnrichment
