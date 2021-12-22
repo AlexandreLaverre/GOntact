ebbe0a87de849defab3159b7c702febbff038c1c
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-12-22 12:55:01 +0100

    Simplify and keep only FDR<0.01

--- scripts/GO_enrichment_contacts/compute.tests.R
+++ scripts/GO_enrichment_contacts/compute.tests.R
@@ -1,46 +1,51 @@
 sp = "human"
-enhancers = "FANTOM5.first150.Tcell.hg38" #"FANTOM5.first150.neuron.enhancers.hg38" 
+enhancers = "FANTOM5.first150.kidney.enhancers.hg38" #"FANTOM5.first150.neuron.enhancers.hg38" 
 GOtype = "complete"
-Baited = "" #".BaitedEnh"
 Trans =  "" #".Trans"
 bait = "" # ".bait2bait"
 
-## Get the total Foreground & Background count in the header
-HeadFile <- file(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"),"r")
-ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-close(HeadFile)
+GOEnrichment.list <- list()
 
-GOEnrichment = read.table(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"), h=T, quote="", sep="\t")
+for (propagation in c("", "before.propagation")){
+  for (Baited in c("", ".BaitedEnh")){
+    
+    GOFile = paste0("/home/laverre/GOntact/results/", sp, "/", propagation, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt")
+  
+    ## Get the total Foreground & Background count in the header
+    HeadFile <- file(GOFile,"r")
+    ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
+    BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
+    close(HeadFile)
+    
+    print(GOFile)
+    GOEnrichment = read.table(GOFile, h=T, quote="", sep="\t")
+    
+    # Hypergeometric test
+    GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
+                                                                  BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
+    # FDR                   
+    GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
+    GOEnrichment = GOEnrichment[which(GOEnrichment$FDR < 10e-3),]
+    
+    propagation.name = ifelse(propagation == "", "after.propagation", "before.propagation")
+    Baited.name = ifelse(Baited == "", "default", "baited.enh")
+    type = paste(propagation.name, Baited.name, sep=".")
+    
+    GOEnrichment.list[[paste0(type, ".bio")]] = GOEnrichment[which(GOEnrichment$GONamespace == "biological_process"),]
+    GOEnrichment.list[[paste0(type,".cell")]] = GOEnrichment[which(GOEnrichment$GONamespace == "cellular_component"),]
+    GOEnrichment.list[[paste0(type,".mol")]] = GOEnrichment[which(GOEnrichment$GONamespace == "molecular_function"),]
+    
+    
+  }
+}
 
-# Hypergeometric test
-GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
-                                                              BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
-# FDR                   
-GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
+#######################################################################################################
+
+View(GOEnrichment.list[["before.propagation.default.bio"]])
+View(GOEnrichment.list[["after.propagation.default.bio"]])
+
+View(GOEnrichment.list[["before.propagation.baited.enh.bio"]])
+View(GOEnrichment.list[["after.propagation.baited.enh.bio"]])
 
 
-GOEnrichment.default.bio = GOEnrichment[which(GOEnrichment$GONamespace == "biological_process"),]
-GOEnrichment.default.cell = GOEnrichment[which(GOEnrichment$GONamespace == "cellular_component"),]
-GOEnrichment.default.mol = GOEnrichment[which(GOEnrichment$GONamespace == "molecular_function"),]
-
-############################
-Baited = ".BaitedEnh"
-
-## Get the total Foreground & Background count in the header
-HeadFile <- file(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"),"r")
-ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-close(HeadFile)
-
-GOEnrichment = read.table(paste0("/home/laverre/GOntact/results/", sp, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt"), h=T, quote="", sep="\t")
-
-# Hypergeometric test
-GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
-                                                              BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
-# FDR                   
-GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
-
-GOEnrichment.baited.bio = GOEnrichment[which(GOEnrichment$GONamespace == "biological_process"),]
-GOEnrichment.baited.cell = GOEnrichment[which(GOEnrichment$GONamespace == "cellular_component"),]
-GOEnrichment.baited.mol = GOEnrichment[which(GOEnrichment$GONamespace == "molecular_function"),]
+#######################################################################################################
