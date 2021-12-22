2755e5cf7b1ec990416c6e4e6f5fd217c473715d
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2021-12-22 19:54:40 +0100

    Distance distribution and add options

--- scripts/GO_enrichment_contacts/compute.tests.R
+++ scripts/GO_enrichment_contacts/compute.tests.R
@@ -1,51 +1,54 @@
+path="/home/laverre/GOntact/results/"
+
 sp = "human"
-enhancers = "FANTOM5.first150.kidney.enhancers.hg38" #"FANTOM5.first150.neuron.enhancers.hg38" 
 GOtype = "complete"
 Trans =  "" #".Trans"
-bait = "" # ".bait2bait"
+bait2bait = "" # ".bait2bait"
+propagation = ".WithoutPropagation"
+
+samples = c("kidney", "neuron", "liver", "heart")
 
 GOEnrichment.list <- list()
 
-for (propagation in c("", "before.propagation")){
-  for (Baited in c("", ".BaitedEnh")){
-    
-    GOFile = paste0("/home/laverre/GOntact/results/", sp, "/", propagation, "/", enhancers, "/", GOtype, ".GO.frequency", Baited, Trans, bait, ".txt")
+for (sample in samples){
+  print(sample)
+  enh=paste("FANTOM5.first1000", sample, "enhancers.hg38", sep=".")
   
-    ## Get the total Foreground & Background count in the header
-    HeadFile <- file(GOFile,"r")
-    ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-    BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
-    close(HeadFile)
-    
-    print(GOFile)
-    GOEnrichment = read.table(GOFile, h=T, quote="", sep="\t")
-    
-    # Hypergeometric test
-    GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"]), as.numeric(x["BackgroundFrequency"]), 
-                                                                  BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
-    # FDR                   
-    GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
-    GOEnrichment = GOEnrichment[which(GOEnrichment$FDR < 10e-3),]
-    
-    propagation.name = ifelse(propagation == "", "after.propagation", "before.propagation")
-    Baited.name = ifelse(Baited == "", "default", "baited.enh")
-    type = paste(propagation.name, Baited.name, sep=".")
-    
-    GOEnrichment.list[[paste0(type, ".bio")]] = GOEnrichment[which(GOEnrichment$GONamespace == "biological_process"),]
-    GOEnrichment.list[[paste0(type,".cell")]] = GOEnrichment[which(GOEnrichment$GONamespace == "cellular_component"),]
-    GOEnrichment.list[[paste0(type,".mol")]] = GOEnrichment[which(GOEnrichment$GONamespace == "molecular_function"),]
-    
-    
-  }
+  GOFile = paste0(path, sp, "/", enh, "/", GOtype, ".GO.frequency", bait2bait, Trans, propagation, ".txt")
+  
+  ## Get the total Foreground & Background count in the header
+  HeadFile <- file(GOFile,"r")
+  ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
+  BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
+  close(HeadFile)
+  
+
+  GOEnrichment = read.table(GOFile, h=T, quote="", sep="\t")
+  
+  # Hypergeometric test
+  GOEnrichment$Pval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"])-1, as.numeric(x["BackgroundFrequency"]), 
+                                                                BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
+  # FDR                   
+  GOEnrichment$FDR = p.adjust(GOEnrichment$Pval, method = "fdr")
+  GOEnrichment = GOEnrichment[which(GOEnrichment$FDR < 10e-3),]
+
+  GOEnrichment.list[[sample]][["bio.process"]] = GOEnrichment[which(GOEnrichment$GONamespace == "biological_process"),]
+  GOEnrichment.list[[sample]][["cell.compo"]] = GOEnrichment[which(GOEnrichment$GONamespace == "cellular_component"),]
+  GOEnrichment.list[[sample]][["mol.funct"]] = GOEnrichment[which(GOEnrichment$GONamespace == "molecular_function"),]
+
 }
 
 #######################################################################################################
+# Distance distribution between fragments
 
-View(GOEnrichment.list[["before.propagation.default.bio"]])
-View(GOEnrichment.list[["after.propagation.default.bio"]])
-
-View(GOEnrichment.list[["before.propagation.baited.enh.bio"]])
-View(GOEnrichment.list[["after.propagation.baited.enh.bio"]])
-
-
-#######################################################################################################
+par(mfrow=c(2,2))
+for (sample in samples){
+  enh=paste("FANTOM5.first1000", sample, "enhancers.hg38", sep=".")
+  
+  ForegroundContact = read.table(paste0(path, sp, "/", enh, "/foreground.contacts.txt"), h=T, sep="\t")
+  hist(ForegroundContact$Distance, breaks=50, main=sample, xlab="Distance")
+  med = median(ForegroundContact$Distance)
+  abline(v=med, col="red")
+  mtext(paste0("med.dist=", med, "\n", "N=", nrow(ForegroundContact)), line = -1)
+  
+}
