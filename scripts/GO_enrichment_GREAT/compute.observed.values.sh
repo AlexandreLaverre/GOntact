f0a3fd12edb5ee262e3b770c681b3dd0b9908e7f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-12 21:47:01 +0100

    lower memory

--- scripts/GO_enrichment_GREAT/compute.observed.values.sh
+++ scripts/GO_enrichment_GREAT/compute.observed.values.sh
@@ -33,7 +33,7 @@ echo "#SBATCH --output=${pathScripts}/log/std_out_obs_${sp}_${method}_${space}" 
 echo "#SBATCH --error=${pathScripts}/log/std_err_obs_${sp}_${method}_${space}" >>  ${pathScripts}/bsub_script_observed
 echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/bsub_script_observed ## 1 CPUs
 echo "#SBATCH --time=3:00:00" >>  ${pathScripts}/bsub_script_observed ## 3 hours
-echo "#SBATCH --mem=20G" >>  ${pathScripts}/bsub_script_observed ## 20g per CPU
+echo "#SBATCH --mem=10G" >>  ${pathScripts}/bsub_script_observed ## 10g per CPU
 
 echo "perl ${pathScripts}/compute.observed.values.pl --pathInputElements=${pathEnhancers}/${sp}/${dataset}.bed --pathBackgroundElements=${pathEnhancers}/${sp}/${background}.bed --pathGOCategories=${pathGO}/GOCategories.txt --pathGOAnnotations=${pathGO}/${sp}.simplified.gene.annotation.${space}.txt --pathRegulatoryRegions=${pathResults}/${sp}/${method}/regulatory_regions_Ensembl${ensrelease}_${space}.txt --pathOutput=${pathResults}/${sp}/${method}/${dataset}/observed_values_Ensembl${ensrelease}_background${background}_${space}.txt  --pathOutputAssociation=${pathResults}/${sp}/${method}/${dataset}/enhancer_GO_association_Ensembl${ensrelease}_${space}.txt " >>  ${pathScripts}/bsub_script_observed
 
