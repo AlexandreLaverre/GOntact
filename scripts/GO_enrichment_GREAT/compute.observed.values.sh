06dfa68634a5d1672c5d0f1662aee5d3b8acf894
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-12 20:59:20 +0100

    run on pbil

--- scripts/GO_enrichment_GREAT/compute.observed.values.sh
+++ scripts/GO_enrichment_GREAT/compute.observed.values.sh
@@ -26,12 +26,25 @@ fi
 
 ####################################################################################
 
-perl ${pathScripts}/compute.observed.values.pl --pathInputElements=${pathEnhancers}/${sp}/${dataset}.bed --pathBackgroundElements=${pathEnhancers}/${sp}/${background}.bed --pathGOCategories=${pathGO}/GOCategories.txt --pathGOAnnotations=${pathGO}/${sp}.simplified.gene.annotation.${space}.txt --pathRegulatoryRegions=${pathResults}/${sp}/${method}/regulatory_regions_Ensembl${ensrelease}_${space}.txt --pathOutput=${pathResults}/${sp}/${method}/${dataset}/observed_values_Ensembl${ensrelease}_background${background}_${space}.txt 
+echo "#!/bin/bash" > ${pathScripts}/bsub_script_observed
+echo "#SBATCH --job-name=exp_${sp}_${method}_${space}" >>  ${pathScripts}/bsub_script_observed
+echo "#SBATCH --partition=normal" >>  ${pathScripts}/bsub_script_observed
+echo "#SBATCH --output=${pathScripts}/log/std_out_obs_${sp}_${method}_${space}" >>  ${pathScripts}/bsub_script_observed
+echo "#SBATCH --error=${pathScripts}/log/std_err_obs_${sp}_${method}_${space}" >>  ${pathScripts}/bsub_script_observed
+echo "#SBATCH --cpus-per-task=1" >>  ${pathScripts}/bsub_script_observed ## 1 CPUs
+echo "#SBATCH --time=3:00:00" >>  ${pathScripts}/bsub_script_observed ## 3 hours
+echo "#SBATCH --mem=20G" >>  ${pathScripts}/bsub_script_observed ## 20g per CPU
+
+echo "perl ${pathScripts}/compute.observed.values.pl --pathInputElements=${pathEnhancers}/${sp}/${dataset}.bed --pathBackgroundElements=${pathEnhancers}/${sp}/${background}.bed --pathGOCategories=${pathGO}/GOCategories.txt --pathGOAnnotations=${pathGO}/${sp}.simplified.gene.annotation.${space}.txt --pathRegulatoryRegions=${pathResults}/${sp}/${method}/regulatory_regions_Ensembl${ensrelease}_${space}.txt --pathOutput=${pathResults}/${sp}/${method}/${dataset}/observed_values_Ensembl${ensrelease}_background${background}_${space}.txt " >>  ${pathScripts}/bsub_script_observed
 
 ####################################################################################
 
 ## do the test
 
-Rscript ${pathScripts}/test.enrichment.R ${sp} ${dataset} ${background} ${method} ${space} > ${pathScripts}/log/test.enrichment.${sp}.${dataset}.${method}.${space}.Rout
+echo "Rscript ${pathScripts}/test.enrichment.R ${sp} ${dataset} ${background} ${method} ${space} > ${pathScripts}/log/test.enrichment.${sp}.${dataset}.${method}.${space}.Rout " >>  ${pathScripts}/bsub_script_observed
+
+####################################################################################
+
+sbatch ${pathScripts}/bsub_script_observed
 
 ####################################################################################
