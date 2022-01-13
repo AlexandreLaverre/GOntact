60eabbfe31d8aa776c63e5b8b0fdcdcb7fc27184
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2022-01-13 15:49:20 +0100

    not running all options

--- scripts/GO_enrichment_contacts/run.all.options.sh
+++ scripts/GO_enrichment_contacts/run.all.options.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #Contacts options
-export ExtendOverlap_options=(0 1000 2500)
-export maxDistance_options=(1000000 2000000)
+export ExtendOverlap_options=(5000)
+export maxDistance_options=(1000000)
 export minDistance_options=(0 25000)
-export UniqueGO_options=(--UniqueGO "")
-export BackgroundEnhancers_options=("" FANTOM5.selection.bed FANTOM5.Laverre2022.bed)
+export BackgroundEnhancers_options=(FANTOM5.Laverre2022.bed)
 
 #GOFrequency options
-export GONameSpace_options=(biological_process molecular_function cellular_component)
-export EnhancerCountOnce_options=(--EnhancerCountOnce "")
-export EnhancerContact_options=(--EnhancerContact "")
-export Background_options=(--BackgroundEnhancers "")
+export GONameSpace_options=(biological_process)
+export EnhancerCountOnce_options=(--EnhancerCountOnce)
+export EnhancerContact_options=(--EnhancerContact)
+export Background_options=(--BackgroundEnhancers)
 
 #Count
 numRound=0
@@ -18,24 +17,24 @@ numRound=0
 for ExtendOverlap in "${ExtendOverlap_options[@]}"; do
 for maxDistance in "${maxDistance_options[@]}"; do
 for minDistance in "${minDistance_options[@]}"; do
-for unique in "${UniqueGO_options[@]}"; do
 for BackgroundEnhancer in "${BackgroundEnhancers_options[@]}"; do
-./foreground.and.background.contacts.py human FANTOM5.first1000.kidney.enhancers.hg38.bed --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} ${UniqueGO} --BackgroundEnhancers ${BackgroundEnhancer}
+./foreground.and.background.contacts.py human FANTOM5.first1000.kidney.enhancers.hg38.bed --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} --BackgroundEnhancers ${BackgroundEnhancer}
 
 #GOFrequency
 for GOName in "${GONameSpace_options[@]}"; do
 for EnhancerCountOnce in "${EnhancerCountOnce_options[@]}"; do
 for EnhancerContact in "${EnhancerContact_options[@]}"; do
-./GO.frequency.py human FANTOM5.first1000.kidney.enhancers.hg38.bed ${GOName} --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} ${UniqueGO} ${EnhancerCountOnce} ${EnhancerContact}
+./GO.frequency.py human FANTOM5.first1000.kidney.enhancers.hg38.bed ${GOName} --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} --UniqueGO ${EnhancerCountOnce} ${EnhancerContact}
 
-if [[ ${BackgroundEnhancer} != "" ]] & [[ ${EnhancerContact} == "--EnhancerContact" ]]; then
-./GO.frequency.py human FANTOM5.first1000.kidney.enhancers.hg38.bed ${GOName} --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} ${UniqueGO} ${EnhancerCountOnce} ${EnhancerContact} --BackgroundEnhancers ${BackgroundEnhancer}
+if [[ "${BackgroundEnhancer}" == "FANTOM5.Laverre2022.bed" ]] && [[ "${EnhancerContact}" == "--EnhancerContact" ]]; then
+echo "${BackgroundEnhancer}"
+./GO.frequency.py human FANTOM5.first1000.kidney.enhancers.hg38.bed ${GOName} --ExtendOverlap ${ExtendOverlap} --maxDistance ${maxDistance} --minDistance ${minDistance} --UniqueGO ${EnhancerCountOnce} ${EnhancerContact} --BackgroundEnhancers ${BackgroundEnhancer}
 fi
+
 numRound=$((numRound+1))
 echo "Number of Round: $numRound"
 echo "##########################"
 
-done
 done
 done
 done
