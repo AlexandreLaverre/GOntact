8a3e5e2f9096340ee85ca1fd89730ff20a8da37e
Alexandre Laverré <alaverre@pbil-deb.univ-lyon1.fr>
2022-01-14 10:16:14 +0100

    Add Binom Test and proportion to output

--- scripts/GO_enrichment_contacts/compute.tests.R
+++ scripts/GO_enrichment_contacts/compute.tests.R
@@ -1,50 +1,46 @@
+##################################################################################
 #!/usr/bin/env Rscript
-args = commandArgs(trailingOnly=TRUE)
-
 library(data.table)
 
-path="/beegfs/data/necsulea/GOntact/results/GO_enrichment_contacts/human/"
+args = commandArgs(trailingOnly=TRUE)
+
 dist = args[1]
 file = args[2]
 
-GOFile = paste0(path, dist, "/FANTOM5.first1000.kidney.enhancers.hg38/GOFrequency/", file)
+pathResult = "/beegfs/data/necsulea/GOntact/results/GO_enrichment_contacts/human/"
+GOFile = paste0(pathResult, dist, "/FANTOM5.first1000.kidney.enhancers.hg38/GOFrequency/", file)
 
+##################################################################################
 ## Get the total Foreground & Background count in the header
 HeadFile <- file(GOFile,"r")
 ForegroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 BackgroundCount = as.numeric(sub(".*= ", "",  readLines(HeadFile,n=1)))
 close(HeadFile)
 
-GOEnrichment = fread(GOFile, h=T, quote="", sep="\t", select = c(1:4))
+##################################################################################
+# GOEnrichment
+GOEnrichment = fread(GOFile, h=T, quote="", sep="\t", select = c(1:4), stringsAsFactors=F)
+GOEnrichment$PropObserved = GOEnrichment$ForegroundFrequency/ForegroundCount
+GOEnrichment$PropExpected = GOEnrichment$BackgroundFrequency/BackgroundCount
 
 # Hypergeometric test
 GOEnrichment$HyperPval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"])-1, as.numeric(x["BackgroundFrequency"]), 
                                                               BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
 
-#GOEnrichment$BinomPval = apply(GOEnrichment, 1, function(x) phyper(as.numeric(x["ForegroundFrequency"])-1, as.numeric(x["BackgroundFrequency"]), 
-#                                                                   BackgroundCount-as.numeric(x["BackgroundFrequency"]), ForegroundCount, lower.tail=FALSE))
+# Binomial test
+GOEnrichment$BinomPval = apply(GOEnrichment, 1, function(x) binom.test(as.numeric(x["ForegroundFrequency"]), ForegroundCount,
+                                                                       as.numeric(x["BackgroundFrequency"])/BackgroundCount,
+                                                                       alternative = "greater")$p.value)
 
 # FDR                   
 GOEnrichment$FDR = p.adjust(GOEnrichment$HyperPval, method = "fdr")
+GOEnrichment$BinomFDR = p.adjust(GOEnrichment$BinomPval, method = "fdr")
 
+##################################################################################
 # Output
-
 GOEnrichment <- GOEnrichment[order(GOEnrichment$FDR),]
-write.table(GOEnrichment, file = paste0(path, dist, "/FANTOM5.first1000.kidney.enhancers.hg38/GOEnrichment/", file),
+write.table(GOEnrichment, file = paste0(pathResult, dist, "/FANTOM5.first1000.kidney.enhancers.hg38/GOEnrichment/", file),
             quote=FALSE, sep = '\t', dec = '.', row.names = F, col.names = T)
 
+##################################################################################
 
-# #######################################################################################################
-# # Distance distribution between fragments
-# 
-# par(mfrow=c(2,2))
-# for (sample in samples){
-#   enh=paste("FANTOM5.first1000", sample, "enhancers.hg38", sep=".")
-#   
-#   ForegroundContact = read.table(paste0(path, sp, "/", enh, "/foreground.contacts.txt"), h=T, sep="\t")
-#   hist(ForegroundContact$Distance, breaks=50, main=sample, xlab="Distance")
-#   med = median(ForegroundContact$Distance)
-#   abline(v=med, col="red")
-#   mtext(paste0("med.dist=", med, "\n", "N=", nrow(ForegroundContact)), line = -1)
-#   
-# }
