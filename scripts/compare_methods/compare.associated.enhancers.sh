f858c0419dda0baeb680fd7275f89677142a0cdc
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-14 08:34:36 +0100

    compare enhancer lists

---
+++ scripts/compare_methods/compare.associated.enhancers.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+export sp=$1
+export dataset=$2
+export method1=$3
+export submethod1=$4
+export method2=$5
+export submethod2=$6
+export space=$7
+
+####################################################################################
+
+export path=/beegfs/data/necsulea/GOntact
+export pathGREAT=${path}/results/GO_enrichment_GREAT/${sp}
+export pathGOntact=${path}/results/GO_enrichment_contacts/${sp}
+export pathResults=${path}/results/method_comparison/${sp}/${dataset}
+export pathScripts=${path}/scripts/compare_methods
+
+export ensrelease=94
+
+####################################################################################
+
+if [ -e ${pathResults} ]; then
+    echo "path output exists"
+else
+    mkdir -p ${pathResults}
+fi
+
+####################################################################################
+
+if [ ${method1} = "GREAT" ]; then
+    export path1=${pathGREAT}/${submethod1}/${dataset}/enhancer_GO_association_Ensembl${ensrelease}_biological_process.txt 
+fi
+
+if [ ${method1} = "GOntact" ]; then
+    export path1=${pathGOntact}/${submethod1}/${dataset}/GOFrequency/uniqueGO.biological_process.Background.EnhancersContactsFANTOM5.Laverre2022.EnhancerScale.EnhancerCountOnce.txt
+fi
+
+####################################################################################
+
+if [ ${method2} = "GREAT" ]; then
+    export path2=${pathGREAT}/${submethod2}/${dataset}/enhancer_GO_association_Ensembl${ensrelease}_biological_process.txt 
+fi
+
+if [ ${method1} = "GOntact" ]; then
+    export path2=${pathGOntact}/${submethod2}/${dataset}/GOFrequency/uniqueGO.biological_process.Background.EnhancersContactsFANTOM5.Laverre2022.EnhancerScale.EnhancerCountOnce.txt
+fi
+
+####################################################################################
+
+perl ${pathScripts}/compare.associated.enhancers.pl --pathGOEnhancers1=${path1} --pathGOEnhancers2=${path2} --pathOutput=${pathResults}/${space}_${method1}.${submethod1}_vs_${method2}.${submethod2}.txt
+
+####################################################################################
