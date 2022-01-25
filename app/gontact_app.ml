c9dcbd891e82ad8009ea20bf3752e845b5e40ca6
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-01-25 11:36:14 +0000

    gontact_app: test for func annot

--- app/gontact_app.ml
+++ app/gontact_app.ml
@@ -18,5 +18,14 @@ let test_gaf () =
     let nb = List.length gl in Printf.printf "%d gene-GO associations in file\n" nb ;
     print_endline (Gaf.show gl)
 
+let test_func_annot () =
+  let g = Gaf.from_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/test_human.gaf" in
+  match g with
+  | Error e -> print_endline e
+  | Ok gl ->
+    let fa = Functional_annotation.from_gaf gl in
+    print_endline (Functional_annotation.show fa `Gene_id_to_go) ;
+    print_endline (Functional_annotation.show fa `Gene_symbol_to_go) ;
+    print_endline (Functional_annotation.show fa `Go_to_gene) 
 
-let () = test_gaf ()
+let () = test_func_annot ()
