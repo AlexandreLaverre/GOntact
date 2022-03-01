b9f808297959b3393f9a365ae33fab63e9d285a9
Philippe Veber <philippe.veber@gmail.com>
2022-03-01 22:27:53 +0100

    whitespacecommit

--- app/gontact_app.ml
+++ app/gontact_app.ml
@@ -1,14 +1,14 @@
 open Core
-open Gontact    
+open Gontact
 
-let test_obo () = 
+let test_obo () =
 let r = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
 match r with
 | Error e -> print_endline e
 | Ok tl ->
   let nb = List.length tl in Printf.printf "%d terms in file\n" nb ;
   print_endline (Obo.show tl)
-  
+
 
 let test_gaf () =
   let g = Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
@@ -24,18 +24,18 @@ let test_gaf () =
   | Error e -> print_endline e
   | Ok gl ->
     let fa = Functional_annotation.of_gaf gl in
-    print_endline (Functional_annotation.show fa `Gene_symbol_to_go) 
-*)    
+    print_endline (Functional_annotation.show fa `Gene_symbol_to_go)
+*)
 
 let test_annot_propagation () =
   let open Let_syntax.Result in
-  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in 
-  let* bp = Ontology.of_obo obo `Biological_process in 
+  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
+  let* bp = Ontology.of_obo obo `Biological_process in
   let+ gaf =  Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
   let fa = Functional_annotation.of_gaf_and_ontology gaf bp in
-  let fap = Functional_annotation.propagate_annotations fa bp in 
+  let fap = Functional_annotation.propagate_annotations fa bp in
   (*let gl = Functional_annotation.extract_genes fa ~go_id:"GO:0000151" `Symbol in
-    print_endline ([%show: string list option] gl) *) 
+    print_endline ([%show: string list option] gl) *)
   Functional_annotation.write_annotations fa "original_annot.txt" ;
   Functional_annotation.write_annotations fap "propagated_annot.txt" ;
   Printf.printf "%d obo elements \n" (List.length obo) ;
@@ -61,11 +61,11 @@ let test_interval_intersection () =
   let int = Genomic_interval_collection.intersect cc1 cc2 in
   String.Map.iteri int ~f:(fun ~key:k ~data:d -> (List.iter d ~f:(fun x -> Printf.printf "%s intersects with %s\n" k x)))
 
-let test_regulatory_domains () = 
+let test_regulatory_domains () =
   let open Let_syntax.Result in
   let* ga = Genomic_annotation.of_ensembl_biomart_file "/home/ubuntu/data/mydatalocal/GOntact/data/ensembl_annotations/human/GeneAnnotation_BioMart_Ensembl102_hg38.txt" in
-  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in 
-  let* bp = Ontology.of_obo obo `Biological_process in 
+  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
+  let* bp = Ontology.of_obo obo `Biological_process in
   let+ gaf =  Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
   let fa = Functional_annotation.of_gaf_and_ontology gaf bp in
   let fap = Functional_annotation.propagate_annotations fa bp in
@@ -78,14 +78,14 @@ let test_regulatory_domains () =
   let filtered_annot = Genomic_annotation.filter_gene_symbols filtered_annot_bio_tx gene_symbols in  (*take only genes whose symbols are in functional (GO) annotations*)
   let domains = Great.basal_plus_extension_domains ~chromosome_sizes:chr_sizes ~genomic_annotation:filtered_annot ~upstream:5000 ~downstream:1000 ~extend:1000000 in
   let int_domains = Great.genomic_interval_collection domains in
-  Genomic_interval_collection.write_output int_domains "basal_plus_extension_domains.txt" ~append:false 
+  Genomic_interval_collection.write_output int_domains "basal_plus_extension_domains.txt" ~append:false
 
 
 let test_go_frequencies () =
   let open Let_syntax.Result in
   let* ga = Genomic_annotation.of_ensembl_biomart_file "/home/ubuntu/data/mydatalocal/GOntact/data/ensembl_annotations/human/GeneAnnotation_BioMart_Ensembl102_hg38.txt" in
-  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in 
-  let* bp = Ontology.of_obo obo `Biological_process in 
+  let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
+  let* bp = Ontology.of_obo obo `Biological_process in
   let+ gaf =  Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
   let fa = Functional_annotation.of_gaf_and_ontology gaf bp in
   let fap = Functional_annotation.propagate_annotations fa bp in
@@ -97,7 +97,7 @@ let test_go_frequencies () =
   let filtered_annot_bio_tx = Genomic_annotation.filter_transcript_biotypes filtered_annot_bio_gene "protein_coding" in (*take only protein_coding transcripts*)
   let filtered_annot = Genomic_annotation.filter_gene_symbols filtered_annot_bio_tx gene_symbols in  (*take only genes whose symbols are in functional (GO) annotations*)
   let domains = Great.basal_plus_extension_domains ~chromosome_sizes:chr_sizes ~genomic_annotation:filtered_annot ~upstream:5000 ~downstream:1000 ~extend:1000000 in
-  let domains_int = Great.genomic_interval_collection domains in 
+  let domains_int = Great.genomic_interval_collection domains in
   let fg = Genomic_interval_collection.of_bed_file "/home/ubuntu/data/mydatalocal/GOntact/data/enhancers/human/FANTOM5.first1000.kidney.enhancers.hg38.bed" ~strip_chr:true in
   let bg = Genomic_interval_collection.of_bed_file "/home/ubuntu/data/mydatalocal/GOntact/data/enhancers/human/FANTOM5.Laverre2022.bed" ~strip_chr:true in
   let go_freq_fg = Great.go_frequencies ~element_coordinates:fg ~regulatory_domains:domains_int ~functional_annot:fap in
