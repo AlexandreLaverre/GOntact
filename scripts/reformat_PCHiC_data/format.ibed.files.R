decaaedacb3237e11e34fa0fe017559b351f7149
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-03-03 13:43:29 +0100

    revert ibed files to original format

---
+++ scripts/reformat_PCHiC_data/format.ibed.files.R
@@ -0,0 +1,30 @@
+###########################################################
+
+path="../../data/PCHi-C/"
+
+###########################################################
+
+for(sp in c("human", "mouse")){
+
+  files=system(paste("ls ",path,sp, "/processed_ibed_files/ | grep ibed",sep=""))
+
+  samples=unlist(lapply(files, function(x) unlist(strsplit(x,split="\\."))[1]))
+
+  for(sample in samples){
+    coords=read.table(paste(path, sp, "/processed_ibed_files/",sample,".ibed",sep=""),h=T, stringsAsFactors=F, sep="\t")
+
+    coords$bait_chr=unlist(lapply(coords$bait_chr, function(x) substr(x,4, nchar(x))))
+    coords$chr=unlist(lapply(coords$chr, function(x) substr(x,4, nchar(x))))
+
+    coords$bait_name=paste(coords$bait_chr, ":",coords$bait_start, "-", coords$bait_end,sep="")
+    coords$otherEnd_name=paste(coords$chr, ":",coords$start, "-", coords$end,sep="")
+
+    coords=coords[,c("bait_chr", "bait_start", "bait_end", "bait_name", "chr", "start", "end", "otherEnd_name", "N_reads", "score")]
+    colnames(coords)=c("bait_chr", "bait_start", "bait_end", "bait_name", "otherEnd_chr", "otherEnd_start", "otherEnd_end", "otherEnd_name", "N_reads", "score")
+
+    write.table(coords, file=paste(path, sp, "/original_ibed_files/",sample,".ibed",sep=""), row.names=F, col.names=T, sep="\t", quote=F)
+
+  }
+}
+
+###########################################################
