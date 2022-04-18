a27e25aa041a6af14257d1420acb6650f1758258
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-04-18 22:32:24 +0200

    corrected syntax

--- scripts/main_figures/data.enrichment.results.R
+++ scripts/main_figures/data.enrichment.results.R
@@ -12,20 +12,20 @@ enrichment.results=list()
 for(sp in c("human", "mouse")){
   enrichment.results[[sp]]=list()
 
-  datasets=system(paste("ls", pathResults, sp,sep=""), intern=T)
+  datasets=system(paste("ls ", pathResults, sp,sep=""), intern=T)
 
   for(data in datasets){
     enrichment.results[[sp]][[data]]=list()
 
-    domains=system(paste("ls", pathResults, sp, "/", data, sep=""), intern=T)
+    domains=system(paste("ls ", pathResults, sp, "/", data, sep=""), intern=T)
 
     for(domain in domains){
       enrichment.results[[sp]][[data]][[domain]]=list()
 
-      methods=system(paste("ls", pathResults, sp, "/", data, "/", domain, sep=""), intern=T)
+      methods=system(paste("ls ", pathResults, sp, "/", data, "/", domain, sep=""), intern=T)
 
       for(method in methods){
-        res=read.table(paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_enrichment_results.txt",sep=""), h=T,sep="\t")
+        res=read.table(paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_enrichment_results.txt",sep=""), h=T,sep="\t", quote="\"")
 
         enrichment.results[[sp]][[data]][[domain]][[method]]=res
       }
