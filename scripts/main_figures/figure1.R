31c391d184a61c161b2e637d92953b480c216d2c
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-04-19 21:40:27 +0200

    graphical details

--- scripts/main_figures/figure1.R
+++ scripts/main_figures/figure1.R
@@ -85,7 +85,10 @@ if(prepare){
   names(all.strands)=c("HIGD1C", "SLC11A2", "CSRNP2", "TFCP2", "POU6F1", "DAZAP2")
 
   all.enhancers=encode[which(encode$Chr==paste("chr",this.chr,sep="") & encode$Start>=xlim[1] & encode$End<=xlim[2]),]
-
+  all.enhancers$ID=paste(all.enhancers$Chr,":", all.enhancers$Start,"-",all.enhancers$End,sep="")
+  all.enhancers$Great=all.enhancers$ID%in%enhancers.great$ElementID
+  all.enhancers$Contacts=all.enhancers$ID%in%enhancers.contacts$ElementID
+  
   prepare=FALSE
 }
 
@@ -101,15 +104,15 @@ pdf(file=paste(pathFigures, "Figure1.pdf",sep=""), width=6.85, height=5.5)
 
 m=matrix(rep(NA, 12*5), nrow=12)
 
-for(i in 1:3){
+for(i in 1:5){
   m[i,]=rep(1,5)
 }
 
-for(i in 4:5){
+for(i in 5:6){
   m[i,]=rep(2,5)
 }
 
-for(i in 6:12){
+for(i in 7:12){
   m[i,]=rep(3,5)
 }
 
@@ -119,9 +122,9 @@ layout(m)
 
 ## plot gene major TSS
 
-ylim=c(-0.3,0.45)
+ylim=c(-0.4,1)
 
-par(mar=c(1,1,2.1,1))
+par(mar=c(0.1, 2.1, 2.1, 1))
 
 plot(1, type="n", xlab="", ylab="", axes=F, main="", xaxs="i", yaxs="i", xlim=xlim, ylim=ylim)
 
@@ -133,7 +136,7 @@ gene.colors["POU6F1"]="red"
 
 tinyy=0.02
 
-ypos.domains=c(-0.485, -0.72, -0.95, -0.485, -0.72, -0.95)
+ypos.domains=c(-0.45, -0.65, -0.9, -0.45, -0.65, -0.9)
 names(ypos.domains)=names(all.tss)
 
 for(g in names(all.tss)){
@@ -142,7 +145,7 @@ for(g in names(all.tss)){
   this.col=gene.colors[g]
 
   if(this.strand=="+"){
-    height=0.25
+    height=0.15
     arrowsize=diff(xlim)/25
 
     basal.start=this.tss-5000
@@ -150,7 +153,7 @@ for(g in names(all.tss)){
   }
 
   if(this.strand=="-"){
-    height=-0.25
+    height=-0.15
     arrowsize=-diff(xlim)/25
     basal.start=this.tss+5000
     basal.end=this.tss-1000
@@ -161,25 +164,35 @@ for(g in names(all.tss)){
   segments(this.tss, 0, this.tss, height, col=this.col)
   arrows(this.tss, height, this.tss+arrowsize, height, length=0.05, col=this.col)
 
-  segments(this.tss, 0, this.tss,ypos.domains[g], col=this.col, xpd=NA, lty=3)
+  if(g=="POU6F1"){
+    text(g, x=this.tss-diff(xlim)/30, y=0.05, adj=c(0.025,0.02), font=3, xpd=NA, col=this.col)
+   }
 }
 
-prettyx=pretty(xlim)
-labels=paste(prettyx/1e6, "Mb", sep=" ")
-axis(side=3, at=prettyx, labels=labels, mgp=c(3,0.5,0))
+text("TSS", x=xlim[2]-diff(xlim)/40, y=0.1, cex=1.1)
 
 #####################################################################################
 
-## plot regulatory domains
+## draw enhancers on the same plot
 
-## gene.ypos=1:length(all.tss)
+yenh=0.75
+height.enh=0.025
+segments(xlim[1], yenh, xlim[2], yenh)
 
-gene.ypos=c(6,5,4,6,5,4)
+text("enhancers", x=xlim[2]-diff(xlim)/22, y=0.9, cex=1.1)
+
+segments(all.enhancers$Start[which(!(all.enhancers$Great | all.enhancers$Contacts))] , yenh-height.enh, all.enhancers$End[which(!(all.enhancers$Great | all.enhancers$Contacts))], yenh+height.enh)
+segments(all.enhancers$Start[which(all.enhancers$Great)] , yenh-height.enh, all.enhancers$End[which(all.enhancers$Great)], yenh+height.enh, col="red")
+segments(all.enhancers$Start[which(all.enhancers$Contacts)] , yenh-height.enh, all.enhancers$End[which(all.enhancers$Contacts)], yenh+height.enh, col="steelblue")
+
+#####################################################################################
+
+## regulatory domains on the same plot
+
+gene.ypos=c(0.55, 0.41, 0.3, 0.55, 0.41, 0.3)
 names(gene.ypos)=names(all.tss)
 
-par(mar=c(1,1,0.25,1))
-
-plot(1, type="n", xlab="", ylab="", axes=F, main="", xaxs="i", yaxs="i", xlim=xlim, ylim=c(3.75,6.25))
+text("domains", x=xlim[2]-diff(xlim)/28, y=0.38, cex=1.1)
 
 for(g in names(all.tss)){
   this.domain.start=this.domains$Start[which(this.domains$GeneID==g)]
@@ -188,11 +201,36 @@ for(g in names(all.tss)){
   this.col=gene.colors[g]
 
   segments(this.domain.start, gene.ypos[g], this.domain.end, gene.ypos[g], col=this.col)
-
-  text(g, x=this.domain.start, y=gene.ypos[g], adj=c(0,-0.2), font=3, xpd=NA)
+  
+  if(g=="POU6F1"){
+    segments(this.domain.start, 0, this.domain.start, 0.75, lty=3, col="red")
+    segments(this.domain.end, 0, this.domain.end, 0.75, lty=3, col="red")
+  }
 }
 
+prettyx=pretty(xlim)
+labels=paste(prettyx/1e6, "Mb", sep=" ")
+axis(side=1, at=prettyx, labels=labels, mgp=c(3,0.5,0))
+
 #####################################################################################
+
+## contacted enhancers
+
+for(i in which(all.enhancers$Contacts)){
+  pos.enh=(all.enhancers$Start[i]+all.enhancers$End[i])/2
+  pos.gene=all.tss["POU6F1"]
+  x.center=(pos.enh+pos.gene)/2
+
+  y.first=0.82
+  y.top=1.15
+  
+  segments(pos.gene, y.first, x.center, y.top, col="lightblue", xpd=NA)
+  segments(x.center, y.top, pos.enh, y.first, col="lightblue", xpd=NA)
+}
+
+## plot label
+
+mtext("A", side=3, line=0.1, at=xlim[1]-diff(xlim)/50, font=2, cex=1.1)
 
 #####################################################################################
 
