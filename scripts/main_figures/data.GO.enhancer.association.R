783f243b5275e3f6f0325eb46a4694f86383ccc5
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-04-22 17:48:07 +0200

    enhancer-GO association

---
+++ scripts/main_figures/data.GO.enhancer.association.R
@@ -0,0 +1,40 @@
+#####################################################################################
+
+pathResults="../../results/"
+pathFigures="../../data_for_publication/figures/"
+
+options(stringsAsFactors=F)
+
+#####################################################################################
+
+foreground.association=list()
+
+for(sp in c("human", "mouse")){
+  foreground.association[[sp]]=list()
+
+  datasets=system(paste("ls ", pathResults, sp,sep=""), intern=T)
+
+  for(data in datasets){
+    foreground.association[[sp]][[data]]=list()
+
+    domains=system(paste("ls ", pathResults, sp, "/", data, sep=""), intern=T)
+
+    for(domain in domains){
+      foreground.association[[sp]][[data]][[domain]]=list()
+
+      methods=system(paste("ls ", pathResults, sp, "/", data, "/", domain, sep=""), intern=T)
+
+      for(method in methods){
+        res.fg=read.table(paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_element_GO_association_foreground.txt",sep=""), h=T, sep="\t", quote="\"")
+
+        foreground.association[[sp]][[data]][[domain]][[method]]=res.fg
+      }
+    }
+  }
+}
+
+#####################################################################################
+
+save(foreground.association, file=paste(pathFigures, "RData/data.foreground.GO.association.RData",sep=""))
+
+#####################################################################################
