6bde679d8ffc3fa2922fd6ccd88e7a0587b6d77f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-04-25 16:09:46 +0200

    abbreviation

--- scripts/main_figures/figure2.R
+++ scripts/main_figures/figure2.R
@@ -61,6 +61,7 @@ if(prepare){
     ## smaller names
 
     great1Mb$GOName[which(great1Mb$GOName=="positive regulation of epithelial cell proliferation")]="positive reg. of epithelial cell proliferation"
+    great1Mb$GOName[which(great1Mb$GOName=="regulation of cellular macromolecule biosynthetic process")]="reg. of cell. macromolecule biosynthetic proc."
     contacts1Mb$GOName[which(contacts1Mb$GOName=="positive regulation of transcription by RNA polymerase II")]="positive reg. of transcription by RNA pol II"
 
     ## save results
