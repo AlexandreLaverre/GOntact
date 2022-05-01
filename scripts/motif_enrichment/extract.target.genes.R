356c90b6a725308025f42f355a9ccfd7512b16b2
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-05-01 16:48:33 +0200

    changed target genes

--- scripts/motif_enrichment/extract.target.genes.R
+++ scripts/motif_enrichment/extract.target.genes.R
@@ -18,58 +18,71 @@ gontact=read.table(paste(pathResults, sp, "/", sample, "/biological_process/cont
 
 ################################################################################
 
-hnf4a=read.table(paste(pathTargetGenes, "Marable2020_HNF4a_knockout.txt",sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"", comment.char="")
+genes=c("HNF4a", "Hoxd13")
+paths=c("Marable2020_HNF4a_knockout.txt", "Salsi2008_Hoxd13.txt")
 
-down=hnf4a$gene[which(hnf4a$log2.fold_change.<0 & hnf4a$q_value<0.05)]
+for(i in 1:length(genes)){
+  gene=genes[i]
+  file=files[i]
+
+  if(gene=="HNF4a"){
+    hnf4a=read.table(paste(pathTargetGenes, file,sep=""), h=T, stringsAsFactors=F, sep="\t", quote="\"", comment.char="")
+
+    targets=toupper(hnf4a$gene[which(hnf4a$log2.fold_change.<0 & hnf4a$q_value<0.05)])
+  } else{
+    targets=readLines(paste(pathTargetGenes, file, sep=""))
+  }
+
+  ################################################################################
+
+  elements.gontact=unique(gontact$ElementID[which(toupper(gontact$GeneSymbol)%in%targets)])
+  elements.great=unique(great$ElementID[which(toupper(great$GeneSymbol)%in%targets)])
+
+  only.gontact=setdiff(elements.gontact, elements.great)
+  only.great=setdiff(elements.great, elements.gontact)
+  shared=intersect(elements.great, elements.gontact)
+
+  ################################################################################
+
+  outdir=gene
+
+  system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/GREAT_only/", sep=""))
+  system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/GOntact_only/", sep=""))
+  system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/shared/", sep=""))
+
+
+  ## write output for only great
+  chr.great=unlist(lapply(only.great, function(x) unlist(strsplit(x, split=":"))[1]))
+  coords.great=unlist(lapply(only.great, function(x) unlist(strsplit(x, split=":"))[2]))
+  start.great=unlist(lapply(coords.great, function(x) unlist(strsplit(x, split="-"))[1]))
+  end.great=unlist(lapply(coords.great, function(x) unlist(strsplit(x, split="-"))[2]))
+
+  res.great=data.frame(chr.great, start.great, end.great, only.great, rep(".", length(only.great)),rep("+", length(only.great)))
+
+  write.table(res.great, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/GREAT_only/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+
+  ## write output for only gontact
+
+  chr.gontact=unlist(lapply(only.gontact, function(x) unlist(strsplit(x, split=":"))[1]))
+  coords.gontact=unlist(lapply(only.gontact, function(x) unlist(strsplit(x, split=":"))[2]))
+  start.gontact=unlist(lapply(coords.gontact, function(x) unlist(strsplit(x, split="-"))[1]))
+  end.gontact=unlist(lapply(coords.gontact, function(x) unlist(strsplit(x, split="-"))[2]))
+
+  res.gontact=data.frame(chr.gontact, start.gontact, end.gontact, only.gontact, rep(".", length(only.gontact)),rep("+", length(only.gontact)))
+
+  write.table(res.gontact, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/GOntact_only/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
+
+  ## write output for shared
+
+  chr.shared=unlist(lapply(shared, function(x) unlist(strsplit(x, split=":"))[1]))
+  coords.shared=unlist(lapply(shared, function(x) unlist(strsplit(x, split=":"))[2]))
+  start.shared=unlist(lapply(coords.shared, function(x) unlist(strsplit(x, split="-"))[1]))
+  end.shared=unlist(lapply(coords.shared, function(x) unlist(strsplit(x, split="-"))[2]))
+
+  res.shared=data.frame(chr.shared, start.shared, end.shared, shared, rep(".", length(shared)),rep("+", length(shared)))
+
+  write.table(res.shared, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/shared/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
 
 ################################################################################
 
-elements.gontact=unique(gontact$ElementID[which(gontact$GeneSymbol%in%down)])
-elements.great=unique(great$ElementID[which(great$GeneSymbol%in%down)])
-
-only.gontact=setdiff(elements.gontact, elements.great)
-only.great=setdiff(elements.great, elements.gontact)
-shared=intersect(elements.great, elements.gontact)
-
-################################################################################
-
-outdir="HNF4a"
-
-system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/GREAT_only/", sep=""))
-system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/GOntact_only/", sep=""))
-system(paste("mkdir -p ",pathResults, "motif_enrichment/",sp,"/",outdir,"/shared/", sep=""))
-
-
-## write output for only great
-chr.great=unlist(lapply(only.great, function(x) unlist(strsplit(x, split=":"))[1]))
-coords.great=unlist(lapply(only.great, function(x) unlist(strsplit(x, split=":"))[2]))
-start.great=unlist(lapply(coords.great, function(x) unlist(strsplit(x, split="-"))[1]))
-end.great=unlist(lapply(coords.great, function(x) unlist(strsplit(x, split="-"))[2]))
-
-res.great=data.frame(chr.great, start.great, end.great, only.great, rep(".", length(only.great)),rep("+", length(only.great)))
-
-write.table(res.great, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/GREAT_only/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
-
-## write output for only gontact
-
-chr.gontact=unlist(lapply(only.gontact, function(x) unlist(strsplit(x, split=":"))[1]))
-coords.gontact=unlist(lapply(only.gontact, function(x) unlist(strsplit(x, split=":"))[2]))
-start.gontact=unlist(lapply(coords.gontact, function(x) unlist(strsplit(x, split="-"))[1]))
-end.gontact=unlist(lapply(coords.gontact, function(x) unlist(strsplit(x, split="-"))[2]))
-
-res.gontact=data.frame(chr.gontact, start.gontact, end.gontact, only.gontact, rep(".", length(only.gontact)),rep("+", length(only.gontact)))
-
-write.table(res.gontact, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/GOntact_only/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
-
-## write output for shared
-
-chr.shared=unlist(lapply(shared, function(x) unlist(strsplit(x, split=":"))[1]))
-coords.shared=unlist(lapply(shared, function(x) unlist(strsplit(x, split=":"))[2]))
-start.shared=unlist(lapply(coords.shared, function(x) unlist(strsplit(x, split="-"))[1]))
-end.shared=unlist(lapply(coords.shared, function(x) unlist(strsplit(x, split="-"))[2]))
-
-res.shared=data.frame(chr.shared, start.shared, end.shared, shared, rep(".", length(shared)),rep("+", length(shared)))
-
-write.table(res.shared, paste(pathResults, "motif_enrichment/",sp,"/",outdir,"/shared/associated_enhancers.bed",sep=""), row.names=F, col.names=F, sep="\t", quote=F)
-
-################################################################################
+}
