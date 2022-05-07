a85d80abb22b8c6002244d6b2cac5208c3753d9f
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-05-07 15:26:17 +0200

    corrected path

--- etc/cmds.compare.sh
+++ etc/cmds.compare.sh
@@ -8,11 +8,11 @@ export domain=$3
 
 #################################################################################
 
- ./_build/install/default/bin/gontact-utils compare-methods --path-annot1 <(zcat results/${sp}/${sample}/${domain}/GREAT_upstream5kb_downstream1kb_extend1Mb/GOntact_element_GO_association_background.txt.gz) --path-annot2 <(zcat results/${sp}/${sample}/${domain}/contacts_mindist0kb_maxdist1Mb/GOntact_element_GO_association_background.txt.gz) --path-output results/${sp}/${sample}/${sample}/compare_GO_assoc_background_GREAT_contacts_1Mb.txt
+ ./_build/install/default/bin/gontact-utils compare-methods --path-annot1 <(zcat results/${sp}/${sample}/${domain}/GREAT_upstream5kb_downstream1kb_extend1Mb/GOntact_element_GO_association_background.txt.gz) --path-annot2 <(zcat results/${sp}/${sample}/${domain}/contacts_mindist0kb_maxdist1Mb/GOntact_element_GO_association_background.txt.gz) --path-output results/${sp}/${sample}/compare_GO_assoc_background_GREAT_contacts_1Mb.txt
 
 #################################################################################
 
- ./_build/install/default/bin/gontact-utils compare-methods --path-annot1 <(zcat results/${sp}/${sample}/${domain}/GREAT_upstream5kb_downstream1kb_extend1Mb/GOntact_element_GO_association_background.txt.gz) --path-annot2 <(zcat results/${sp}/${sample}/${domain}/simulated_contacts_mindist0kb_maxdist1Mb/GOntact_element_GO_association_background.txt.gz) --path-output results/${sp}/${sample}/${sample}/compare_GO_assoc_background_GREAT_simulated_contacts_1Mb.txt
+ ./_build/install/default/bin/gontact-utils compare-methods --path-annot1 <(zcat results/${sp}/${sample}/${domain}/GREAT_upstream5kb_downstream1kb_extend1Mb/GOntact_element_GO_association_background.txt.gz) --path-annot2 <(zcat results/${sp}/${sample}/${domain}/simulated_contacts_mindist0kb_maxdist1Mb/GOntact_element_GO_association_background.txt.gz) --path-output results/${sp}/${sample}/compare_GO_assoc_background_GREAT_simulated_contacts_1Mb.txt
 
 
 #################################################################################
