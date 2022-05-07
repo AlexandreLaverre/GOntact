4d4cd7abef7b7b6e49d7c76e559e3b40a437b04b
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-05-07 14:59:49 +0200

    save object by sp and data

--- scripts/main_figures/data.gene.enhancer.association.R
+++ scripts/main_figures/data.gene.enhancer.association.R
@@ -7,24 +7,17 @@ options(stringsAsFactors=F)
 
 #####################################################################################
 
-background.association=list()
-foreground.association=list()
-
 for(sp in c("human", "mouse")){
-  background.association[[sp]]=list()
-  foreground.association[[sp]]=list()
-
   datasets=system(paste("ls ", pathResults, sp,sep=""), intern=T)
 
   for(data in datasets){
-    background.association[[sp]][[data]]=list()
-    foreground.association[[sp]][[data]]=list()
 
     domains=system(paste("ls ", pathResults, sp, "/", data, sep=""), intern=T)
 
     for(domain in domains){
-      background.association[[sp]][[data]][[domain]]=list()
-      foreground.association[[sp]][[data]][[domain]]=list()
+
+      background.association=list()
+      foreground.association=list()
 
       methods=system(paste("ls ", pathResults, sp, "/", data, "/", domain, sep=""), intern=T)
 
@@ -35,22 +28,22 @@ for(sp in c("human", "mouse")){
           res.bg=read.table(path.bg, h=T, sep="\t", quote="\"")
         } else{
           path.bg.gz=paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_element_gene_association_background.txt.gz",sep="")
-          res.bg=read.table(gzfile(path.bg.gz, open="r"), h=T, sep="\t", quote="\"")
+          conn=gzfile(path.bg.gz, open="r")
+          res.bg=read.table(conn, h=T, sep="\t", quote="\"")
+          close(conn)
         }
 
-        background.association[[sp]][[data]][[domain]][[method]]=res.bg
+        background.association[[method]]=res.bg
 
         res.fg=read.table(paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_element_gene_association_foreground.txt",sep=""), h=T, sep="\t", quote="\"")
 
-        foreground.association[[sp]][[data]][[domain]][[method]]=res.fg
+        foreground.association[[method]]=res.fg
+
+        save(background.association, file=paste(pathFigures, "RData/data.background.association.",sp,".",data, ".",domain,".RData",sep=""))
+        save(foreground.association, file=paste(pathFigures, "RData/data.foreground.association.",sp,".",data, ".", domain, ".RData",sep=""))
       }
     }
   }
 }
-
-#####################################################################################
-
-save(background.association, file=paste(pathFigures, "RData/data.background.association.RData",sep=""))
-save(foreground.association, file=paste(pathFigures, "RData/data.foreground.association.RData",sep=""))
 
 #####################################################################################
