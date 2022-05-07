1a349753cfe7d71bf9780638cf59620374feb31a
Anamaria Necsulea <anamaria.necsulea@univ-lyon1.fr>
2022-05-07 15:53:08 +0200

    exclude txt files

--- scripts/main_figures/data.enrichment.results.R
+++ scripts/main_figures/data.enrichment.results.R
@@ -22,7 +22,7 @@ for(sp in c("human", "mouse")){
     for(domain in domains){
       enrichment.results[[sp]][[data]][[domain]]=list()
 
-      methods=system(paste("ls ", pathResults, sp, "/", data, "/", domain, sep=""), intern=T)
+      methods=system(paste("ls ", pathResults, sp, "/", data, "/", domain, " | grep -v txt", sep=""), intern=T)
 
       for(method in methods){
         res=read.table(paste(pathResults, sp, "/", data, "/", domain, "/", method, "/GOntact_enrichment_results.txt",sep=""), h=T,sep="\t", quote="\"")
