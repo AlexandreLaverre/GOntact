7e0e581471ec3b6550fbcb8643691bab4a1e38f1
Philippe Veber <philippe.veber@gmail.com>
2022-05-19 09:21:36 +0200

    app: disable unused value check for test functions

--- app/gontact_app.ml
+++ app/gontact_app.ml
@@ -2,7 +2,7 @@ open Cmdliner
 open Core
 open Gontact
 
-let test_obo () =
+let _test_obo () =
 let r = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
 match r with
 | Error e -> print_endline e
@@ -11,7 +11,7 @@ match r with
   print_endline (Obo.show tl)
 
 
-let test_gaf () =
+let _test_gaf () =
   let g = Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
   match g with
   | Error e -> print_endline e
@@ -19,7 +19,7 @@ let test_gaf () =
     let nb = List.length gl in Printf.printf "%d gene-GO associations in file\n" nb ;
     print_endline (Gaf.show gl)
 
-let test_annot_propagation () =
+let _test_annot_propagation () =
   let open Let_syntax.Result in
   let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
   let* bp = Ontology.of_obo obo Biological_process in
@@ -31,20 +31,20 @@ let test_annot_propagation () =
   Printf.printf "%d obo elements \n" (List.length obo) ;
   Printf.printf "%d gaf elements \n" (List.length gaf)
 
-let test_overlap_coordinates () =
+let _test_overlap_coordinates () =
   let baits = Genomic_interval_collection.of_bed_file "data/PCHi-C/human/hg38.baitmap" ~strip_chr:false ~format:Base1 in
   let tss = Genomic_interval_collection.of_bed_file "tss_coords_1000.txt" ~strip_chr:false ~format:Base1 in
   let intersect = Genomic_interval_collection.intersect baits tss in
   String.Map.iteri intersect ~f:(fun ~key ~data -> Printf.printf "%s\t%s\n" key (String.concat ~sep:"," data)) ;
   Genomic_interval_collection.write_output baits "ordered_baits.txt" ~append:false ;
-  Genomic_interval_collection.write_output tss "ordered_tss.txt" ~append:false 
-    
-let test_merge_coordinates () =
+  Genomic_interval_collection.write_output tss "ordered_tss.txt" ~append:false
+
+let _test_merge_coordinates () =
   let cc = Genomic_interval_collection.of_bed_file "test.bed" ~strip_chr:false ~format:Base0 in
   let merged = Genomic_interval_collection.merge_coordinates cc in
   print_endline (Genomic_interval_collection.show merged)
 
-let test_genomic_annot () =
+let _test_genomic_annot () =
   let open Let_syntax.Result in
   let+ ga = Genomic_annotation.of_ensembl_biomart_file "/home/ubuntu/data/mydatalocal/GOntact/data/ensembl_annotations/human/GeneAnnotation_BioMart_Ensembl102_hg38.txt" in
   let filtered1 = Genomic_annotation.filter_gene_biotypes ga "protein_coding" in
@@ -52,13 +52,13 @@ let test_genomic_annot () =
   let major_isoforms = Genomic_annotation.identify_major_isoforms filtered2 in
   String.Map.iteri major_isoforms ~f:(fun ~key:k ~data:d -> Printf.printf "gene %s major isoform %s\n" k d)
 
-let test_interval_intersection () =
+let _test_interval_intersection () =
   let cc1 = Genomic_interval_collection.of_bed_file "test1.bed" ~strip_chr:false ~format:Base1 in
   let cc2 = Genomic_interval_collection.of_bed_file "test2.bed" ~strip_chr:false ~format:Base1 in
   let int = Genomic_interval_collection.intersect cc1 cc2 in
   String.Map.iteri int ~f:(fun ~key:k ~data:d -> (List.iter d ~f:(fun x -> Printf.printf "%s intersects with %s\n" k x)))
 
-let test_regulatory_domains () =
+let _test_regulatory_domains () =
   let open Let_syntax.Result in
   let* ga = Genomic_annotation.of_ensembl_biomart_file "/home/ubuntu/data/mydatalocal/GOntact/data/ensembl_annotations/human/GeneAnnotation_BioMart_Ensembl102_hg38.txt" in
   let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
@@ -84,7 +84,7 @@ let test_go_frequencies () =
   let* obo = Obo.of_obo_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/go-basic.obo" in
   let* bp = Ontology.of_obo obo Biological_process in
   let+ gaf =  Gaf.of_gaf_file "/home/ubuntu/data/mydatalocal/GOntact/data/GeneOntology/goa_human.gaf" in
-  let gonames = Ontology.term_names bp in 
+  let gonames = Ontology.term_names bp in
   let fa = Functional_annotation.of_gaf_and_ontology gaf bp in
   let fap = Functional_annotation.propagate_annotations fa bp in
   let gene_symbols = Functional_annotation.gene_symbols fap in
@@ -100,17 +100,15 @@ let test_go_frequencies () =
   let bg = Genomic_interval_collection.of_bed_file "/home/ubuntu/data/mydatalocal/GOntact/data/enhancers/human/FANTOM5.Laverre2022.bed" ~strip_chr:true ~format:Base0 in
   let go_frequencies_foreground = Great.go_frequencies ~element_coordinates:fg ~regulatory_domains:domains_int ~functional_annot:fap in
   let go_frequencies_background = Great.go_frequencies ~element_coordinates:bg ~regulatory_domains:domains_int ~functional_annot:fap in
-  let enrichment_results = Go_enrichment.foreground_vs_background_binom_test ~go_frequencies_foreground ~go_frequencies_background in 
+  let enrichment_results = Go_enrichment.foreground_vs_background_binom_test ~go_frequencies_foreground ~go_frequencies_background in
   Go_enrichment.write_output enrichment_results gonames "test_enrichment_great.txt"
 *)
-    
-let test_fdr () =
+
+let _test_fdr () =
   let pvalues = [ ("p1", 0.0015) ; ("p2", 0.3) ; ("p3", 0.1) ; ("p4", 0.05) ; ("p5", 0.005) ; ("p6", 0.001) ; ("p7", 0.005) ] in
   let fdr = Stats.false_discovery_rates pvalues in
-  List.iter fdr ~f:(fun (id, pval) -> Printf.printf "id %s fdr %f\n" id pval) 
+  List.iter fdr ~f:(fun (id, pval) -> Printf.printf "id %s fdr %f\n" id pval)
 
 (* let res = test_go_frequencies ()  *)
-    
+
 let () = exit @@ Cmd.eval CLI.command
-
-
