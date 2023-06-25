94f4765000093e91936a400e7da3023604df28a8
Philippe Veber <philippe.veber@gmail.com>
2023-06-25 16:01:00 +0200

    white space commit

--- lib/functional_annotation.ml
+++ lib/functional_annotation.ml
@@ -1,27 +1,27 @@
 open Core
-    
+
 type t = {
   gene_symbol_to_go : string list String.Map.t ;
   gene_id_to_go : string list String.Map.t ;
   go_to_gene_id : string list String.Map.t ;
-  go_to_gene_symbol : string list String.Map.t ;  
+  go_to_gene_symbol : string list String.Map.t ;
 }
 
 let extract_terms ga is =
-  let d, k = 
+  let d, k =
     match is with
     | `Id id ->  ga.gene_id_to_go, id
     | `Symbol sym -> ga.gene_symbol_to_go, sym
   in
-  String.Map.find d k 
+  String.Map.find d k
 
 let extract_terms_exn  ga is =
-  let d, k = 
+  let d, k =
     match is with
     | `Id id ->  ga.gene_id_to_go, id
     | `Symbol sym -> ga.gene_symbol_to_go, sym
   in
-  String.Map.find_exn d k 
+  String.Map.find_exn d k
 
 let extract_genes ga ~go_id:go i =
   let d = match i with
@@ -36,7 +36,7 @@ let of_gaf_and_ontology (gaf:Gaf.t) (ont:Ontology.t) =
   let no_empty_symbols = List.filter gaf_without_no ~f:(fun ga -> not (String.equal ga.gene_symbol "")) in
   let make_dict fg f compare =
     let m = String.Map.of_alist_multi (List.map fg ~f) in
-    String.Map.map m ~f:(List.dedup_and_sort ~compare) 
+    String.Map.map m ~f:(List.dedup_and_sort ~compare)
   in
   let gene_symbol_to_go = make_dict no_empty_symbols (fun ga -> ga.gene_symbol, ga.go_id) String.compare in
   let gene_id_to_go = make_dict gaf_without_no (fun ga -> (ga.gene_id, ga.go_id)) String.compare in
@@ -46,8 +46,8 @@ let of_gaf_and_ontology (gaf:Gaf.t) (ont:Ontology.t) =
 
 let show fa d =
   match d with
-    | `Gene_id_to_go -> String.Map.to_alist fa.gene_id_to_go |> [%show: (string * string list) list] 
-    | `Gene_symbol_to_go -> String.Map.to_alist fa.gene_symbol_to_go |>  [%show: (string * string list) list] 
+    | `Gene_id_to_go -> String.Map.to_alist fa.gene_id_to_go |> [%show: (string * string list) list]
+    | `Gene_symbol_to_go -> String.Map.to_alist fa.gene_symbol_to_go |>  [%show: (string * string list) list]
     | `Go_to_gene_id -> String.Map.to_alist fa.go_to_gene_id |>  [%show: (string * string list) list]
     | `Go_to_gene_symbol -> String.Map.to_alist fa.go_to_gene_symbol |>  [%show: (string * string list) list]
 
@@ -57,12 +57,12 @@ let reverse_dict gtg =
   let flatten_list (k, l) =
     List.fold l ~init:[] ~f:(fun ll x -> (x, k)::ll)
   in
-  let reval = List.fold al ~init:[] ~f:(fun ll x -> List.append (flatten_list x) ll) in 
-  String.Map.of_alist_multi reval 
+  let reval = List.fold al ~init:[] ~f:(fun ll x -> List.append (flatten_list x) ll) in
+  String.Map.of_alist_multi reval
 
 let propagate_annotations fa o =
   let filter_and_extend dict =
-    let filtered = String.Map.map dict ~f:(fun l -> Ontology.filter_terms o l) in  (* for each gene, extract GO ids that belong to this ontology *) 
+    let filtered = String.Map.map dict ~f:(fun l -> Ontology.filter_terms o l) in  (* for each gene, extract GO ids that belong to this ontology *)
     String.Map.map filtered ~f:(fun l -> Ontology.expand_id_list o l) (*expand id list *)
   in
   let extended_gig = filter_and_extend fa.gene_id_to_go in
@@ -73,15 +73,15 @@ let propagate_annotations fa o =
 
 
 let write_annotations fa path =
-  let s_to_go = fa.gene_symbol_to_go in 
+  let s_to_go = fa.gene_symbol_to_go in
   let output = Out_channel.create ~append:false path in
-  let write_entry k l = 
-     List.iter l ~f:(Printf.fprintf output "%s\t%s\n" k) 
+  let write_entry k l =
+     List.iter l ~f:(Printf.fprintf output "%s\t%s\n" k)
   in
-  Out_channel.output_string output "GeneSymbol\tGOID\n" ; 
-  String.Map.iteri s_to_go ~f:(fun ~key:k ~data:l -> write_entry k l) ; 
-  Out_channel.close output 
-  
+  Out_channel.output_string output "GeneSymbol\tGOID\n" ;
+  String.Map.iteri s_to_go ~f:(fun ~key:k ~data:l -> write_entry k l) ;
+  Out_channel.close output
+
 let gene_symbols fa =
   String.Map.keys fa.gene_symbol_to_go
   |> String.Set.of_list
@@ -90,5 +90,4 @@ let go_list_of_gene_symbol fa gs =
   let symbol2go = fa.gene_symbol_to_go in
   match String.Map.find symbol2go gs with
   | Some l -> l
-  | None -> [] 
-            
+  | None -> []
--- lib/functional_annotation.mli
+++ lib/functional_annotation.mli
@@ -5,12 +5,12 @@ type t
 val show : t -> [ `Gene_id_to_go | `Gene_symbol_to_go | `Go_to_gene_symbol | `Go_to_gene_id ] -> string 
 
 val extract_terms : t -> [ `Id of string | `Symbol of string ] -> string list option
-    
-val extract_terms_exn : t -> [ `Id of string | `Symbol of string ] -> string list 
+
+val extract_terms_exn : t -> [ `Id of string | `Symbol of string ] -> string list
 
 val extract_genes : t -> go_id:string -> [ `Id | `Symbol ] -> string list option
 
-val of_gaf_and_ontology : Gaf.t -> Ontology.t -> t   
+val of_gaf_and_ontology : Gaf.t -> Ontology.t -> t
 
 val propagate_annotations : t -> Ontology.t -> t
 
@@ -18,4 +18,4 @@ val write_annotations : t -> string -> unit
 
 val gene_symbols : t -> String.Set.t 
 
-val go_list_of_gene_symbol : t -> string -> string list 
+val go_list_of_gene_symbol : t -> string -> string list
