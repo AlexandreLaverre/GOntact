4187fe94fc1d21f0ee7890867904ae6ef6194ba0
Philippe Veber <philippe.veber@gmail.com>
2023-07-01 23:15:43 +0200

    server: minimally working form

--- server/gontact_server.ml
+++ server/gontact_server.ml
@@ -1,171 +1,201 @@
-open Printf
+open Tyxml
 
-module H = Tyxml.Html
-
-(* module Form = struct *)
-(*   type _ t = *)
-(*     | Return : 'a -> 'a t *)
-(*     | Map : ('a -> 'b) * 'a t -> 'b t *)
-(*     | Both : 'a t * 'b t -> ('a * 'b) t *)
-(*     | Checkbox : { name : string ; label : string ; values : string list } -> string t *)
-
-(*   let return x = Return x *)
-(*   let map f x = Map (f, x) *)
-(*   let checkbox ~name ~label values = Checkbox { label ; name ; values } *)
-
-(*   let rec to_elts : type s. s t -> _ Tyxml.Html.elt list = function *)
-(*     | Return _ -> [] *)
-(*     | Map (_, x) -> to_elts x *)
-(*     | Both (a, b) -> to_elts a @ to_elts b *)
-(*     | Checkbox cbx -> *)
-(*       Tyxml.Html.[ *)
-(*         fieldset *)
-(*           ~legend:(legend [txt cbx.label]) *)
-(*           (List.map (fun v -> input ~a:[a_name cbx.name ; a_input_type `Radio ; a_value v] ()) cbx.values) ; *)
-(*       ] *)
-(* end *)
-
-(* module Form_decoding = struct *)
-(*   let ( let* ) = Result.bind *)
-(*   let ( let+ ) x f = Result.map f x *)
-(*   let string args name = *)
-(*     match List.assoc_opt name args with *)
-(*     | None -> Error (Printf.sprintf "missing arg %s" name) *)
-(*     | Some v -> Ok v *)
-
-(*   let run = function *)
-(*     | Ok x -> x *)
-(*     | Error msg -> Dream.html ~status:`Bad_Request msg *)
-(* end *)
-
-
-module Form = struct
-  open Html_types
-
-  type _ t =
-    (* | Return : 'a -> ('a, _) t *)
-    | Map : ('a -> 'b) * 'a t -> 'b t
-    | Both : 'a t * 'b t -> ('a * 'b) t
-    | Radio : { name : string ; label : string ;
-                encode : 'a -> string ;
-                decode : string -> 'a ;
-                values : (string * 'a) list } -> 'a t
-    | Textarea : { name : string ; label : string } -> string t
-    | File : { name : string ; label : string } -> string t
-
-  (* let return x = Return x *)
-  (* let map f x = Map (f, x) *)
-  let radio ~name ~label ~encode ~decode values = Radio { label ; name ; values ; encode ; decode }
-  let textarea ~name ~label = Textarea { name ; label }
-  let file ~name ~label = File {name ; label}
-
-  let ( let+ ) x f = Map (f, x)
-  let ( and+ ) x y = Both (x, y)
-
-  let rec to_elts : type u. u t -> [> div] Tyxml.Html.elt list = function
-    (* | Return _ -> [] *)
-    | Map (_, x) -> to_elts x
-    | Both (a, b) -> to_elts a @ to_elts b
-    | Radio cbx ->
-      let open Tyxml.Html in
-      let mk_input i (str_repr, v) =
-        let id = sprintf "%s-%d" cbx.name i in
-        let lab = label ~a:[a_label_for id ; a_class ["form-check-label"]] [txt str_repr] in
-        div ~a:[a_class ["form-check"]] [
-          input ~a:[a_name cbx.name ; a_id id ; a_input_type `Radio ;
-                    a_class ["form-check-input"]; a_value (cbx.encode v)] () ;
-          lab ;
-        ]
-      in
-      [ div ~a:[a_class ["mb-3"]] ((label [txt cbx.label]) :: (List.mapi mk_input cbx.values)) ]
-    | Textarea { label = l ; name } ->
-      let open Tyxml.Html in
-      [
-        div ~a:[a_class ["mb-3"]] [
-          label ~a:[a_label_for name ; a_class ["form-label"]] [txt l] ;
-          br () ;
-          textarea ~a:[a_name name] (txt "") ;
-        ]
-      ]
-    | File {name ; label = l} ->
-      let open Tyxml.Html in
-      [
-        div ~a:[a_class ["mb-3"]] [
-          label ~a:[a_label_for name ; a_class ["form-label"]] [txt l] ;
-          input ~a:[a_name name ; a_input_type `File ; a_class ["form-control"]] ()
-        ]
-      ]
-
-  let to_elt request x =
-    let open Tyxml.Html in
-    form ~a:[a_method `Post ; a_class ["w-50"]] [
-      Unsafe.data @@ Dream.csrf_tag request ;
-      div (to_elts x) ;
-      input ~a:[a_class ["btn";"btn-primary"]; a_value "Submit"] () ;
-    ]
-end
-
-let css_link ?a href = H.link ?a ~rel:[`Stylesheet] ~href ()
-let ext_js_script ?(a = []) href = H.script ~a:(H.a_src href :: a) (H.txt "")
+let css_link ?a href = Html.link ?a ~rel:[`Stylesheet] ~href ()
+(* let ext_js_script ?(a = []) href = Html.script ~a:(Html.a_src href :: a) (Html.txt "") *)
 
 let html_page ~title body =
-  H.html
-    (H.head (H.title (H.txt title)) [
-        H.meta ~a:[H.a_charset "utf-8"] () ;
-        css_link "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" ~a:[H.a_integrity "sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" ; H.a_crossorigin `Anonymous] ;
-      ])
-    (H.body [H.div ~a:[H.a_class ["container"]] body])
+  let head =
+    Html.head (Html.title (Html.txt title)) [
+      Html.meta ~a:[Html.a_charset "utf-8"] () ;
+      Html.meta ~a:[Html.a_http_equiv "X-UA-Compatible" ; Html.a_content "IE=edge"] () ;
+      Html.meta ~a:[Html.a_name "viewport" ; Html.a_content "width=device-width, initial-scale=1"] () ;
+      Html.link ~rel:[`Icon] ~href:"img/favicon.ico" () ;
+      css_link "css/marx.css" ;
+      css_link "css/gontact.css" ;
+    ]
+  in
+  Html.html ~a:[Html.a_lang "en"] head (Html.body [Html.main body])
 
 module Form_page = struct
-  let main_form =
-    let open Form in
-    let+ species_assembly =
-      radio ~name:"species-assembly" ~label:"Species assembly" ~encode:Fun.id ~decode:Fun.id [
-        "hg38", "hg38" ;
-        "mm10", "mm10" ;
-      ]
-    and+ test_region_data = textarea ~name:"test-region-data" ~label:"Enter test regions (BED format)"
-    and+ test_region_file = file ~name:"test-region-file" ~label:"Or upload a BED file"
-    in
-    (species_assembly, test_region_data, test_region_file)
+  let%html intro_par = {|
+    <p style="text-align:justify;">
+      GOntact aims to evaluate Gene Ontology (GO) enrichments for sets of
+      noncoding elements. To do this, GOntact uses chromatin contacts determined
+      with the promoter capture Hi-C (PCHi-C) approach. GO categories
+      are "transmitted" to noncoding elements from the genes that they
+      are contacted by in the PCHi-C data. The statistical
+      significance of GO enrichments is evaluated
+      with binomial tests, which compare the frequency of elements
+      associated with a given GO category in a foreground set with the
+      frequency of elements associated with the same GO category in a
+      background set.
+      By default, the coordinates of enhancer elements predicted by
+      ENCODE are used as a background set. For the GOntact webserver, PCHi-C chromatin contacts
+      are those compiled by Laverre <i>et al.</i>, Genome Research,
+      2022. The data used in this version of the GOntact webserver are
+      described <a href="GOntact_data_current.html">here</a>.
+      If you want to run GOntact on a different organism or a
+      different set of PCHi-C samples, please see our command-line
+      version on GitLab. If you want to run GREAT using the same
+      genome annotations and Gene Ontology data, you can do so
+      <a href="GREAT.html">here</a>.
+    </p>|}
+
+  let%html gontact_form request = {|
+  <form method="Post" action = "/">
+    |}[Tyxml.Html.Unsafe.data @@ Dream.csrf_tag request]{|
+    <!-- GO domain choice -->
+      Select the <b>Gene Ontology domain</b> for which you want to
+      compute an enrichment:
+      <br>
+      <input type="radio" id="biological_process" name="domain-choice" value="biological_process" />
+      <label for="biological_process">biological process</label>
+      <br>
+      <input type="radio" id="molecular_function" name="domain-choice" value="molecular_function" />
+      <label for="molecular_function">molecular function</label>
+      <br>
+       <input type="radio" id="cellular_component" name="domain-choice" value="cellular_component" />
+      <label for="cellular_component">cellular component</label>
+      <br>
+
+      <hr>
+
+      <!-- genome choice -->
+      Select the <b>genome</b> you want to analyze:
+      <br>
+      <input type="radio" id="genome_human" name="genome-choice" value="human" />
+      <label for="genome_human">human (hg38)</label>
+      <br>
+
+      <input type="radio" id="genome_mouse" name="genome-choice" value="mouse" />
+      <label for="genome_mouse">mouse (mm10)</label>
+      <hr>
+
+      <!-- foreground regions -->
+
+      <label for="foreground-file">Input the coordinates of the <b>foreground</b> regions (bed format):</label>
+      <br>
+      <input type="file" id="foreground-file" name="foreground-file">
+
+      <!-- background regions -->
+
+      <br>
+
+      <label for="background-file">(Optional) Input the coordinates of
+      a custom set of <b>background</b> regions (bed format):</label>
+      <br>
+      <input type="file" id="background-file" name="background-file">
+
+      <!-- Chromatin contact parameters-->
+
+      <br>
+      <hr>
+      Selection of <b>chromatin interactions</b>:
+      <br>
+    	<table style="padding:0px">
+	  <!-- nb samples-->
+	  <tr>
+	    <td>
+           <label for="min-samples"> Minimum number
+	of PCHi-C samples: </label>
+	    </td>
+	    <td>
+	      <input type="text"
+		     name="min-samples"  value="1" style="width: 100px">
+	    </td>
+	  </tr>
+
+	   <!-- nb samples-->
+	  <tr>
+	    <td>
+              <label for="min-score"> Minimum CHiCAGO score: </label>
+	    </td>
+	    <td>
+	      <input type="text"
+		     name="min-score"  value="5" style="width: 100px">
+	    </td>
+	  </tr>
+
+	  <!-- min distance-->
+	  <tr>
+	    <td>
+	      <label for="min-dist-contacts"> Minimum distance between
+	      contacted regions (bp):</label>
+	    </td>
+	     <td>
+	      <input type="text"
+		     name="min-dist-contacts" value="25000" style="width: 100px">
+	     </td>
+	  </tr>
+
+	  <!-- max distance-->
+	  <tr>
+	    <td>
+	      <label for="max-dist-contacts"> Maximum distance between
+	      contacted regions (bp):</label>
+	    </td>
+	     <td>
+	      <input type="text"
+		     name="max-dist-contacts"  value="1000000" style="width: 100px">
+	     </td>
+	     </tr>
+
+	  <!-- max overlap-->
+	  <tr>
+	    <td>
+	      <label for="max-dist-element-fragment"> Maximum distance between
+	      foreground regions and contacted fragments (bp):</label>
+	    </td>
+	     <td>
+	      <input type="text"
+		     name="max-dist-element-fragment"  value="5000" style="width: 100px">
+	     </td>
+	     </tr>
+
+
+	  </table>
+
+      <!-- submit button-->
+      <div><input type="submit"></div>
+
+    </form>|}
 
   let render request =
     let open Tyxml.Html in
     html_page ~title:"GOntact" [
-      h1 [txt "GOntact"] ;
-      Form.to_elt request main_form ;
-      ext_js_script "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ~a:[H.a_integrity "sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" ; H.a_crossorigin `Anonymous] ;
+      header [
+        img ~src:"img/GOntact_logo.png" ~alt:"GOntact logo" ~a:[a_width 600] () ;
+      ] ;
+      h1 [small [txt "Gene Ontology enrichments based on chromatin contacts for noncoding elements"]] ;
+      hr () ;
+      intro_par ;
+      hr () ;
+      gontact_form request ;
     ]
 end
 
-(* let species_assembly_name = "species-assembly" *)
+let analysis
+    ~background_file ~domain_choice ~foreground_file
+    ~genome_choice ~max_dist_contacts ~max_dist_element_fragment
+    ~min_dist_contacts ~min_samples ~min_score =
+  (* let _background_file = match background_file with *)
+  (*   | "" -> None *)
+  (*   | _ -> Some background_file *)
+  (* in *)
+  [ background_file ;
+    domain_choice ;
+    foreground_file ;
+    genome_choice ;
+    max_dist_contacts ;
+    max_dist_element_fragment ;
+    min_dist_contacts ;
+    min_samples ; min_score ]
+  |> Lwt.return
 
-(* let form_page request = *)
-(*   let open Tyxml.Html in *)
-(*   html_page ~title:"GOntact" [ *)
-(*     h1 [txt "GOntact"] ; *)
-(*     form ~a:[a_method `Post] [ *)
-(*       Unsafe.data @@ Dream.csrf_tag request ; *)
-(*       (\* fieldset *\) *)
-(*       (\*   ~legend:(legend [txt "Species assembly"]) *\) *)
-(*       (\*   [ *\) *)
-(*       div ~a:[a_class ["form-check"]] [ *)
-(*         input ~a:[a_id "assembly-hg38" ; a_name species_assembly_name ; a_class ["form-check-input"]; a_input_type `Radio ; a_value "hg38" ] () ; *)
-(*         label ~a:[a_label_for "assembly-hg38" ; a_class ["form-label"]] [ txt "Human genome" ] ; *)
-(*       ] ; *)
-(*       div ~a:[a_class ["form-check"]] [ *)
-(*         input ~a:[a_id "assembly-mm10" ; a_name species_assembly_name ; a_class ["form-check-input"]; a_input_type `Radio ; a_value "mm10" ] () ; *)
-(*         label ~a:[a_label_for "assembly-mm10" ; a_class ["form-label"]] [ txt "Human genome" ] ; *)
-(*       ] ; *)
-(*       input ~a:[a_input_type `Submit] () ; *)
-(*     ] ; *)
-(*     ext_js_script "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" ~a:[H.a_integrity "sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" ; H.a_crossorigin `Anonymous] ; *)
-(*   ] *)
-
-let result_page ~species_assembly _request =
+let generate_result_page xs =
   let open Tyxml.Html in
-  html_page ~title:"GOntact" [
-    txt species_assembly ;
+  html_page ~title:"GOntact results" [
+    ul (List.map (fun x -> li [txt x]) xs)
   ]
 
 let html_to_string html =
@@ -177,14 +207,34 @@ let () =
   @@ Dream.memory_sessions
   @@ Dream.router [
 
-    Dream.get  "/"
-      (fun request ->
-        Dream.html (html_to_string @@ Form_page.render request));
+    Dream.get "/css/**" @@ Dream.static "server/static/css" ;
+    Dream.get "/img/**" @@ Dream.static "server/static/img" ;
+
+    Dream.get  "/" (fun request ->
+        Dream.html (html_to_string @@ Form_page.render request)
+      );
 
     Dream.post "/" (fun request ->
         match%lwt Dream.form request with
-        | `Ok ["message", _msg ; "species-assembly", species_assembly] ->
-          Dream.html (html_to_string (result_page ~species_assembly request))
+        | `Ok [
+            "background-file", background_file ;
+            "domain-choice", domain_choice ;
+            "foreground-file", foreground_file ;
+            "genome-choice", genome_choice ;
+            "max-dist-contacts", max_dist_contacts ;
+            "max-dist-element-fragment", max_dist_element_fragment ;
+            "min-dist-contacts", min_dist_contacts ;
+            "min-samples", min_samples ;
+            "min-score", min_score
+          ] ->
+          let%lwt res =
+            analysis
+              ~background_file ~domain_choice ~foreground_file
+              ~genome_choice ~max_dist_contacts ~max_dist_element_fragment
+              ~min_dist_contacts ~min_samples ~min_score
+          in
+          let page = generate_result_page res in
+          Dream.html (html_to_string page)
         | _ ->
           Dream.empty `Bad_Request);
 
